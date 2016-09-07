2800bd1035b4d305d3f0eb4f61b2489ece0517d3
Prashant Aher <prashantaher70@gmail.com>
2016-09-08 00:13:24 +0530

    Synonys enhancements

--- controller/HomeController.js
+++ controller/HomeController.js
@@ -3,12 +3,14 @@ omni = angular.module("OmniVocab")
 omni.controller('HomeController', ['$state', '$scope', '$timeout', '$mdToast',
     function($state, $scope, $timeout, $mdToast){
 
+    //this might not be always correct, like synonyms, antonyms
     $scope.allWords = []
+    //always correct
     $scope.allWordsMap = {}
     $scope.wordToBeAdded = {}
 
     $scope.loadWordsFromStorage = function() {
-        chrome.storage.sync.get("all-words", function (data) {
+        chrome.storage.local.get("all-words", function (data) {
             $scope.allWordsMap = data["all-words"]
 
             if($scope.allWordsMap == undefined) {
@@ -23,7 +25,7 @@ omni.controller('HomeController', ['$state', '$scope', '$timeout', '$mdToast',
             }
         })
     }
-    
+
     $scope.loadWordsFromStorage()
 
     $scope.addSynonym = function() {
@@ -48,6 +50,8 @@ omni.controller('HomeController', ['$state', '$scope', '$timeout', '$mdToast',
         meaning = $scope.wordToBeAdded.meaning
         updatedWordsForMsg = ''
 
+        allSynonymsStr = new Set()
+
         mainWord = {
             'word': word,
             'meaning': meaning
@@ -55,8 +59,8 @@ omni.controller('HomeController', ['$state', '$scope', '$timeout', '$mdToast',
 
         allWords = []
         allWords.push(mainWord)
+        allSynonymsStr.add(angular.lowercase(mainWord.word))
 
-        console.log(synonyms)
         synonyms.forEach(function(synonym) {
             synonymItem = {
                 'word': synonym.word
@@ -69,27 +73,65 @@ omni.controller('HomeController', ['$state', '$scope', '$timeout', '$mdToast',
             }
 
             allWords.push(synonymItem)
+            allSynonymsStr.add(angular.lowercase(synonymItem.word))
         })
 
+        toStoreWords = []
+
+        //get synonyms of existing words
         allWords.forEach(function(item) {
-            wordToStore = item
-            
+
             wordExists = false
-            existingWord = $scope.allWordsMap[angular.lowercase(wordToStore.word)]
+            existingWord = $scope.allWordsMap[angular.lowercase(item.word)]
 
             if(existingWord != undefined) {
                 wordExists = true
-                updatedWordsForMsg = (updatedWordsForMsg == '') ? wordToStore.word : (updatedWordsForMsg + ', ' + wordToStore.word)
+                existingWord.synonyms.forEach(function(syn) {
+                    allSynonymsStr.add(angular.lowercase(syn))
+                })
             }
 
-            $scope.allWordsMap[angular.lowercase(item.word)] = wordToStore
-            
-            if(!existingWord) {
-                $scope.allWords.push(wordToStore)
+            toStoreWords.push(item)
+        })
+
+        //other words updated
+        allSynonymsStr.forEach(function(syn) {
+            alreadyQueuedObjs = toStoreWords.filter(function(o) {
+                return angular.lowercase(o.word) == syn
+            })
+
+            if(alreadyQueuedObjs.length == 0) {
+                otherUpdated = $scope.allWordsMap[syn]
+                if(otherUpdated != undefined) {
+                    console.log("Not queued for update nut needs update" + otherUpdated.word)
+                    toStoreWords.push(otherUpdated)
+                }
             }
         })
 
-        chrome.storage.sync.set({"all-words": $scope.allWordsMap}, function() {
+        //update or store
+        toStoreWords.forEach(function(item) {
+            item.synonyms = []
+            allSynonymsStr.forEach(function(syn) {
+                item.synonyms.push(syn)
+            })
+
+            wordExists = false
+            existingWord = $scope.allWordsMap[angular.lowercase(item.word)]
+
+            if(existingWord != undefined) {
+                wordExists = true
+                updatedWordsForMsg = (updatedWordsForMsg == '') ? item.word : (updatedWordsForMsg + ', ' + item.word)
+            }
+
+            $scope.allWordsMap[angular.lowercase(item.word)] = item
+            
+            if(!wordExists) {
+                $scope.allWords.push(item)
+            }
+        })
+
+        chrome.storage.local.set({"all-words": $scope.allWordsMap}, function() {
             msg = 'Saved'
             if(updatedWordsForMsg != '') {
                 msg = 'Updated ' + updatedWordsForMsg
@@ -120,16 +162,19 @@ omni.controller('HomeController', ['$state', '$scope', '$timeout', '$mdToast',
 
         },
         'querySearch': function(query) {
-            query = angular.lowercase(query)
-            function createFilterFor(query) {
-                return function filterFn(item) {
-                    return (
-                        angular.lowercase(item.word).indexOf(query) === 0
-                    );
-                };
-            }
+            function filter(words, query) {
+                query = angular.lowercase(query)
+                function createFilterFor(query) {
+                    return function filterFn(item) {
+                        return (
+                            angular.lowercase(item.word).indexOf(query) === 0
+                        );
+                    };
+                }
 
-            var results = query ? $scope.allWords.filter( createFilterFor(query) ) : []
+                return words.filter( createFilterFor(query) )
+            }
+            var results = query ? filter($scope.allWords, query) : []
             return results;
         }
     }
